0eda32607ecf77358f1d5dd1363bad1d59e5df28
Sabbir Ahmed <sabbirahmedcse33@gmail.com>
2020-09-20 15:40:47 +0600

    Update Print Function.py

--- Practice and Uses/HackerRank/Print Function.py
+++ Practice and Uses/HackerRank/Print Function.py
@@ -6,3 +6,10 @@ if __name__ == '__main__':
 
     st = ''.join(str(e) for e in li)
     print(st)
+    
+    
+ # easy one  
+ if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i+1,end="")
