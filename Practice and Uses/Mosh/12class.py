11b5f955a51c39a6eb71f487b80857a08730dea6
ArafatSabbir <sabbirahmedcse33@gmail.com>
2020-10-11 14:33:07 +0600

    Update class

--- Practice and Uses/Mosh/12class.py
+++ Practice and Uses/Mosh/12class.py
@@ -1,13 +1,12 @@
 class MyFormulas:
-    def square(self):
-        print("is a num")
+    def square(self,num):
+        print(f'Square is {num*num}')
 
 
-  #  def area_of_circle(r):
-        #area = 3.1416*r**2
-   #     print(f'Area of circle is {r}') 
+    def area_of_circle(self,r):
+        print(f'Area of circle is {3.1416*r**2}') 
 
 
 obj1 = MyFormulas()
-#obj1.area_of_circle(5)
-obj1.square()
+obj1.area_of_circle(5)
+obj1.square(10)
